0d135053f63a80fa23a8de2af7eb5e3a9496fea6
Jason Jacobson <jason.g.jacobson@gmail.com>
2019-02-26 22:04:14 -0500

    Menu button added to non-index pages, set up code for front end of schedule checkbox; need to add backend support.

--- ClassScheduling/Scripts/dropdown.js
+++
@@ -1,7 +0,0 @@
-﻿var menu = document.getElementsByClassName("dropdown");
-
-if (window.location.pathname == "/home/index" || window.location.pathname == "/home/info"){
-    menu.style.visibility = "hidden";
-} else {
-    menu.style.visibility = "visible";
-}
